08e6b900ed3cb3850559c8175cdbff0787a498c3
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-11 09:35:41 +0530

    50

--- 1_Learn Introductory JS by building a Pyramid Generator/script.js
+++ 1_Learn Introductory JS by building a Pyramid Generator/script.js
@@ -4,5 +4,11 @@ const rows = [];
 for (let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i));
 }
+function padRow() {
+    
+}
+const call = padRow();
+console.log(call);
+
 let result = "";
 console.log(result);
