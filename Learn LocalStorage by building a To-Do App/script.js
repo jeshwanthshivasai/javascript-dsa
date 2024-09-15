e0edd7bc49b41409e024c9f49a979d173d35e0d3
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-15 18:36:26 +0530

    deleted to-do app

--- Learn LocalStorage by building a To-Do App/script.js
+++
@@ -1,3 +0,0 @@
-const taskForm = document.getElementById('task-form');
-const confirmCloseDialog = document.getElementById('confirm-close-dialog');
-const openTaskFormBtn = document.getElementById('open-task-form-btn');
