adc143b411830fb2e4a070cd86e35f031054c661
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-15 18:17:48 +0530

    js intro part 1 done ✅

--- 1_Learn Introductory JS by building a Pyramid Generator/script.js
+++ 1_Learn Introductory JS by building a Pyramid Generator/script.js
@@ -3,6 +3,11 @@ const count = 8;
 const rows = [];
 let inverted = true;
 
+// const character = "!";
+// const count = 10;
+// const rows = [];
+// let inverted = false;
+
 function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
