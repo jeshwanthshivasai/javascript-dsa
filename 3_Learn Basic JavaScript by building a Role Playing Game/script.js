247ca6f214c0a94b188e02e1bb32c5415478a552
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-18 22:10:52 +0530

    55

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -16,12 +16,13 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#monsterStats');
 const monsterName = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
-const locations = [];
 
-const cat =  {
-
-}
-console.log(cat);
+const locations = [
+    {
+      name: "town square",
+      "button text": [],
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
