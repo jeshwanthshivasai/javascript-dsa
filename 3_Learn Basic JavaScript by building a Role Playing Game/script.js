ea84bf67a130a518d7f8f707b4b1347e262356ae
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-19 22:33:48 +0530

    59

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -19,8 +19,16 @@ const monsterHealthText = document.querySelector('#monsterHealth');
 
 const locations = [
     {
-      name: "town square",
-      "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        text: "You are in the town square. You see a sign that says \"Store\".",
+        "button functions": [goStore, goCave, fightDragon],
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
     }
 ];
 
