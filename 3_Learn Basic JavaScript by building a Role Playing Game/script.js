490bfd5babaf53af454b734feba6444083cc5eba
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-09-27 22:44:31 +0530

    87

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -102,6 +102,7 @@ function buyWeapon() {
         gold-=30;
         currentWeaponIndex ++;
         goldText.innerText = gold;
+        let newWeapon = weapons;
         text.innerText = "You now have a new weapon."
     }
 }
