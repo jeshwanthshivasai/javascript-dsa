973ae200336aa5fdae39f1dfc289034cad02b5cd
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-06 12:12:58 +0530

    11

--- 23_Learn Fetch & Promises by building an FCC Authors Page/script.js
+++ 23_Learn Fetch & Promises by building an FCC Authors Page/script.js
@@ -16,6 +16,7 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
 
 const displayAuthors = (authors) => {
     authors.forEach(({author, image, url, bio}, index) => {
-
+        const authorContainer = document.getElementById('author-container');
+        authorContainer.innerHTML += ``;
     });
 }
