7ca44bf93b60f23f28cf305487a9ae77784cb3e9
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-17 22:41:42 +0530

    121

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -121,7 +121,8 @@ function goFight() {
 }
 
 function attack() {
-    text.innerText = 'The ' + monsters[fighting].name + ' attacks.'
+    text.innerText = 'The ' + monsters[fighting].name + ' attacks.';
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
  }
 
 function dodge() {
