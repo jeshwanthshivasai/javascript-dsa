e11f7f8a596ae11d3b3e55993bc80da30e64a45e
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-19 09:28:27 +0530

    127

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -129,6 +129,8 @@ function attack() {
     monsterHealthText.innerText = monsterHealth;
     if(health <= 0) {
         lose();
+    } else if (monsterHealth <= 0) {
+        defeatMonster();
     }
  }
 
