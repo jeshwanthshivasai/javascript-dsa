09b64c85b0fbac47b0228e7de5c48b60cd142ced
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-19 13:15:57 +0530

    169

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -239,6 +239,9 @@ function pick(guess) {
     for(let i=0; i<10; i++) {
         text.innerText += numbers[i] + "\n";
     }
+    if(numbers.includes(guess)) {
+
+    }
 }
 
 function buyHealth() {
