02f73f6ec7ba20b130c964d03e7f40de352ca410
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-19 10:20:05 +0530

    144

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -142,7 +142,7 @@ function goFight() {
 function attack() {
     text.innerText = 'The ' + monsters[fighting].name + ' attacks.';
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-    health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
     weapons -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
