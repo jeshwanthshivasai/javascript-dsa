ec899fbe46652379f37a9c5159b5679233477c49
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2024-10-19 09:25:46 +0530

    126

--- 3_Learn Basic JavaScript by building a Role Playing Game/script.js
+++ 3_Learn Basic JavaScript by building a Role Playing Game/script.js
@@ -127,6 +127,9 @@ function attack() {
     weapons -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
+    if(health <= 0) {
+        lose();
+    }
  }
 
 function dodge() {
