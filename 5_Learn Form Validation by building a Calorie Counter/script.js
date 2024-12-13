4bfe5d5e33962e6a6798a859be67b2229df32a84
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-12-13 23:16:59 +0530

    86

--- 5_Learn Form Validation by building a Calorie Counter/script.js
+++ 5_Learn Form Validation by building a Calorie Counter/script.js
@@ -53,8 +53,11 @@ function calculateCalories(e) {
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
   const surplusOrDeficit = (remainingCalories < 0) ? "Surplus" : "Deficit";
-  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>`;
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
   <hr></hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  `;
 }
 
 function getCaloriesFromInputs(list) {
