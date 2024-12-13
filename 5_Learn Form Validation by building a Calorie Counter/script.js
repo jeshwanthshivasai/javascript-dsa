2f12291d261ca2ed54f4f7799a755d128bf60459
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-12-13 09:24:04 +0530

    62

--- 5_Learn Form Validation by building a Calorie Counter/script.js
+++ 5_Learn Form Validation by building a Calorie Counter/script.js
@@ -34,5 +34,6 @@ function getCaloriesFromInputs(list) {
   let calories = 0;
   for(const item of list) {
     const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
   }
 }
