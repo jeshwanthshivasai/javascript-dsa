b42a39ab189e323cb2e065224e071f7900d1fee2
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-12-16 10:24:58 +0530

    2

--- 6_Review DOM Manipulation by building Rock, Paper, Scissors Game/script.js
+++ 6_Review DOM Manipulation by building Rock, Paper, Scissors Game/script.js
@@ -2,8 +2,22 @@ function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
   const randomOption = Math.floor(Math.random() * options.length);
   return options[randomOption];
-  Math.floor(Math.random(options[0])) + 1;
-  Math.floor(Math.random(options[1])) + 1;
-  Math.floor(Math.random(options[2])) + 1;
 }
+
+function hasPlayerWonTheGame() {
+  if((player === 'Rock' && computer === 'Scissors') || (player === 'Scissors' && computer === 'Paper') || (player == 'Paper' && computer == 'Rock')) {
+    return true;
+  }
+  return false;
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+console.log(hasPlayerWonTheRound("Scissors", "Paper"));
+console.log(hasPlayerWonTheRound("Paper", "Rock"));
+console.log(hasPlayerWonTheRound("Paper", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+
 console.log(getRandomComputerResult());
+
